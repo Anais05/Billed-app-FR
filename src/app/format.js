81b7dba118f40435ddd42769e739d14388aa6be8
Anais Assamoi <anaisassamoi02@gmail.com>
2021-10-01 15:57:27 +0200

    correction error dashboard formatDate

--- src/app/format.js
+++ src/app/format.js
@@ -1,5 +1,11 @@
 export const formatDate = (dateStr) => {
+
   const date = new Date(dateStr)
+
+  if (!(date instanceof Date) || isNaN(date)) {
+    return date;
+  }
+  
   const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
   const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
   const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
